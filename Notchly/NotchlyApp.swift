3f22783c1f9d57bf76f5a4ceb53d9b148a14c3c4
gronk2 <markopana2007@gmail.com>
2026-06-24 17:31:42 +0300

    Initial Commit

---
+++ Notchly/NotchlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchlyApp.swift
+//  Notchly
+//
+//  Created by Markos Panayotopoulos on 24/6/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
